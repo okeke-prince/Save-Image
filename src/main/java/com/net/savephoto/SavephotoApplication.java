9e3e9efa773eeed1a1d19d5abca0ce5acbfd362d
Udo <princeinme128@gmail.com>
2023-10-04 00:36:29 +0100

    updates

--- src/main/java/com/net/savephoto/SavephotoApplication.java
+++ src/main/java/com/net/savephoto/SavephotoApplication.java
@@ -1,7 +1,10 @@
 package com.net.savephoto;
 
+import lombok.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class SavephotoApplication {
